d2234881a323b438d09bb3329f3d20ce986b26bf
Pratik <prtksawant09@gmail.com>
2023-06-08 17:51:06 +0530

    testing branch rule.

--- src/main/java/edu/pratik/Executor.java
+++ src/main/java/edu/pratik/Executor.java
@@ -31,7 +31,7 @@ public class Executor {
         executor.factoryDemo();
         executor.loggerDemo();
         executor.proxyDemo();
-        //test
+        //tests
     }
 
     private void proxyDemo() {
